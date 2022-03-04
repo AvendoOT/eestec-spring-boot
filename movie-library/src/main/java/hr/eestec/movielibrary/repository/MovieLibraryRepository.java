2a1a2adb3145517dff277a18c403fad1356aa288
avendo <leon.kranjcevic@outlook.com>
2022-03-04 01:09:12 +0100

    Init project

--- movie-library/src/main/java/hr/eestec/movielibrary/repository/MovieLibraryRepository.java
+++
@@ -1,9 +0,0 @@
-package hr.eestec.movielibrary.repository;
-
-import hr.eestec.movielibrary.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovieLibraryRepository extends JpaRepository<Movie, Long> {
-}
